ae7020b75c7aef83aaae659de416de003e64d9bd
unknown <talyta1405@gmail.com>
2022-09-25 22:38:14 -0300

    Primeiro commit

---
+++ app/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login,logout
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+#formulario de cadastro de usuarios
+def create(request):
+    return render(request, 'create.html')
+
+#insercao dos dados dos usuarios no banco
+def store(request):
+    data={}
+    #Se as senhas forem diferentes
+    if(request.POST['password'] != request.POST['password-conf']):
+        data['msg'] = 'Senha e confirmação de senha diferentes!'
+        data['class'] = 'alert-danger'
+    else:
+    #Inserção dos dados no banco de dados
+        user= User.objects.create_user(request.POST['name'], request.POST['email'], request.POST['password'])
+        user.first_name = request.POST['name']
+        user.save()
+        data['msg'] = 'Usuário cadastrado com sucesso!'
+        data['class'] = 'alert-success'
+    return render(request, 'create.html', data)
+
+#formulario de Login de usuarios
+def painel(request):
+    return render(request, 'painel.html')
+
+#Processar o Login
+def dologin(request):
+    data = {}
+    user = authenticate(username=request.POST['user'], password=request.POST['password'])
+    if user is not None:
+        login(request, user)
+        return redirect('/dashboard/')
+    else:
+        data['msg'] = 'Usuário ou Senha incorretos!'
+        data['class'] = 'alert-danger'
+        return render(request, 'painel.html', data)
+
+#Pagina inicial do dashBoard
+def dashboard(request):
+    return render(request, 'dashboard/home.html')
+
+#Logout do sistema
+def logouts(request):
+    logout(request)
+    return redirect('/painel/')
+
+#Alterar senha
+def changePassword(request):
+    user = User.objects.get(email=request.user.email)
+    user.set_password(request.POST['password'])
+    user.save()
+    logout(request)
+    return redirect('/painel/')
+
+def doPerfil(request):
+    return render(request, 'home.html')
+
